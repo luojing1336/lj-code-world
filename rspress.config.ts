14846898359259b41ad151d83fcfc92f3953d2ae
luojing1336 <luojing1336@hotmail.com>
2025-07-29 10:10:44 +0800

    feat: update logo and favicon files; remove unused logos

--- rspress.config.ts
+++ rspress.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from 'rspress/config';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'lj的代码自留地',
-  icon: '/lj-light-logo.png',
+  icon: '/icon-lj-light-logo-192.png',
   logo: {
-    light: '/lj-light-logo.png',
-    dark: '/lj-dark-logo.png',
+    light: '/icon-lj-light-logo-192.png',
+    dark: '/icon-lj-dark-logo-192.png',
   },
   themeConfig: {
     socialLinks: [
